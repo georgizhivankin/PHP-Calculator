3e53bc5bd66e756f454c3f6f748d639043d730be
Georgi Zhivankin <georgijivankin@gmail.com>
2015-02-24 01:58:44 +0000

    Interface for the different operators classes

--- src/Operations/OperatorsInterface.php
+++ src/Operations/OperatorsInterface.php
@@ -3,4 +3,12 @@ namespace Zhivankin\DeskPROCalculator\Operations;
 
 interface OperatorsInterface
 {
+
+    /**
+     * runOperation function that each operators' class should implement
+     *
+     * @param int $number            
+     * @param int $result            
+     */
+    public function runOperation($number, $result);
 }
